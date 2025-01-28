09f670ab7047296089da008f65de8f0433b0b158
alexis <alexian260696@gmail.com>
2025-01-27 20:11:59 -0600

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     host: '0.0.0.0', // Esto hace que Vite escuche en todas las interfaces de red
-//     port: 3000,       // Puedes cambiar el puerto si es necesario
-//   },
-// })
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/Herbel_React/', // Ruta base correspondiente al nombre del repositorio
+  base: '/Herbel_React/', 
   plugins: [react()],
-});
+  server: {
+    host: '0.0.0.0', // Esto hace que Vite escuche en todas las interfaces de red
+    port: 3000,   
+
+    // Puedes cambiar el puerto si es necesario
+  },
+})
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   base: '/Herbel_React/', // Ruta base correspondiente al nombre del repositorio
+//   plugins: [react()],
+// });
